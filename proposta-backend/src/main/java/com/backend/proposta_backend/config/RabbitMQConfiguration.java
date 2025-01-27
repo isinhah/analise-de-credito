2cdc6da7e3a7d576dcac4550420ec4b2cb0b3192
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-01-27 18:50:44 -0300

    criação da exchange de proposta concluida

--- proposta-backend/src/main/java/com/backend/proposta_backend/config/RabbitMQConfiguration.java
+++ proposta-backend/src/main/java/com/backend/proposta_backend/config/RabbitMQConfiguration.java
@@ -22,7 +22,10 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfiguration {
 
     @Value("${spring.rabbitmq.exchanges.proposta-pendente}")
-    private String exchange;
+    private String exchangePropostaPendente;
+
+    @Value("${spring.rabbitmq.exchanges.proposta-concluida}")
+    private String exchangePropostaConcluida;
 
     @Bean
     public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
@@ -56,7 +59,12 @@ public class RabbitMQConfiguration {
 
     @Bean
     public FanoutExchange criarFanoutExchangePropostaPendente() {
-        return ExchangeBuilder.fanoutExchange(exchange).build();
+        return ExchangeBuilder.fanoutExchange(exchangePropostaPendente).build();
+    }
+
+    @Bean
+    public FanoutExchange criarFanoutExchangePropostaConcluida() {
+        return ExchangeBuilder.fanoutExchange(exchangePropostaConcluida).build();
     }
 
     @Bean
@@ -69,6 +77,18 @@ public class RabbitMQConfiguration {
     public Binding criarBindingPropostaPendenteMsNotificacao() {
         return BindingBuilder.bind(criarFilaPropostaPendenteMsNotificacao())
                 .to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaConcluidaMsPropostaApp() {
+        return BindingBuilder.bind(criarFilaPropostaConcluidaMsProposta())
+                .to(criarFanoutExchangePropostaConcluida());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaConcluidaMsNotificacao() {
+        return BindingBuilder.bind(criarFilaPropostaConcluidaMsNotificacao())
+                .to(criarFanoutExchangePropostaConcluida());
     }
 
     @Bean
