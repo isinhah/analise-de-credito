3ab1624137ebe172541c6693d2c0542c5d203c0a
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-01-29 17:36:22 -0300

    método para atualizar campos especificos da proposta

--- proposta-backend/src/main/java/com/backend/proposta_backend/repository/PropostaRepository.java
+++ proposta-backend/src/main/java/com/backend/proposta_backend/repository/PropostaRepository.java
@@ -1,6 +1,9 @@
 package com.backend.proposta_backend.repository;
 
 import com.backend.proposta_backend.entity.Proposta;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,9 @@ import java.util.List;
 @Repository
 public interface PropostaRepository extends CrudRepository<Proposta, Long> {
     List<Proposta> findAllByIntegradaIsFalse();
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE proposta SET aprovada = :aprovada, observacao = :observacao WHERE id = :id", nativeQuery = true)
+    void atualizarProposta(Long id, boolean aprovada, String observacao);
 }
